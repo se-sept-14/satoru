e53dec8e673e12cb587d78a76eb9970f5fcba74f
kashifulhaque <haque.kashiful7@gmail.com>
2023-11-11 11:18:12 +0530

    Built a docker compose file to streamline deployment process

--- server/deploy.sh
+++ server/deploy.sh
@@ -1,10 +1,3 @@
 #!/bin/bash
 
-# Generate a timestamp (YYYYMMDDHHMMSS format)
-timestamp=$(date +"%Y%m%d%H%M%S")
-
-# Build the docker image
-docker build -t "satoru-api_$timestamp" .
-
-# Run the docker container
-docker run -d --name satoru-api_$timestamp -p 5000:5000 "satoru-api_$timestamp"
+docker compose up -d
--- server/docker-build.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-if [ "$#" -ne 1 ]; then
-  echo "Image built at $0 <timestamp>"
-  exit 1
-fi
-
-timestamp="$1"
-docker build -t "satoru-api_$timestamp" .
