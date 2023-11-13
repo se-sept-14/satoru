b3f423cda32bd511496420a2aa91e26901e14fac
kashifulhaque <haque.kashiful7@gmail.com>
2023-11-13 23:54:35 +0530

    Made a barebones landing page

--- client/src/components/NavbarComponent.vue
+++ client/src/components/NavbarComponent.vue
@@ -1,6 +1,7 @@
 <template>
   <nav
-    class="bg-slate-900 text-white p-3 mx-80 mt-4 rounded-xl shadow-lg shadow-slate-500/30 flex flex-row justify-center items-center"
+    class="text-white p-3 mx-80 mt-4 rounded-xl shadow-lg shadow-slate-500/30 flex flex-row justify-center items-center"
+    style="background-color: #00050d"
   >
     <router-link
       class="basis-1/4 font-sans text-lg cursor-pointer font-medium hover:font-bold mx-2"
@@ -8,12 +9,16 @@
       >recommender system</router-link
     >
     <div class="basis-1/2 flex flex-row">
-      <router-link to="/" class="mx-3 hover:underline">Discover Courses</router-link>
+      <router-link to="/" class="mx-3 hover:underline"
+        >Discover Courses</router-link
+      >
       <router-link to="/" class="mx-3 hover:underline">My Courses</router-link>
-      <router-link to="/" class="mx-3 hover:underline">Recommendations</router-link>
+      <router-link to="/" class="mx-3 hover:underline"
+        >Recommendations</router-link
+      >
     </div>
     <button
-      class="basis-1/6 bg-teal-800 p-2 rounded-lg shadow-md hover:shadow-lg hover:shadow-slate-500/30 hover:text-md"
+      class="basis-1/6 bg-white text-black p-2 rounded-lg shadow-md hover:shadow-lg hover:shadow-yellow-200/90 hover:text-md"
     >
       Register
     </button>
--- client/src/pages/HomePage.vue
+++ client/src/pages/HomePage.vue
@@ -1,5 +1,26 @@
 <template>
-  <div class="container mx-auto mt-4">
-    <h1 class="text-white">This is the home page!</h1>
+  <div class="container mx-auto mt-24 text-white w-6/12 text-center">
+    <div class="flex flex-col">
+      <h1 class="text-6xl font-bold">
+        Get Personalized Course Recommendations for Students
+      </h1>
+      <p class="mt-4 text-xl">
+        Welcome to our website, where you can discover the perfect courses for
+        your educational journey
+      </p>
+
+      <div class="mt-12 flex flex-row justify-center">
+        <button
+          class="mx-1 outline outline-1 p-4 hover:bg-white hover:text-black rounded-md"
+        >
+          Login
+        </button>
+        <button
+          class="mx-1 outline outline-1 p-4 rounded-md bg-white text-black hover:shadow-lg hover:shadow-yellow-200/90"
+        >
+          Register
+        </button>
+      </div>
+    </div>
   </div>
 </template>
