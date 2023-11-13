0dd4d372ca20c992a31b0362caa29db17bf53228
kashifulhaque <haque.kashiful7@gmail.com>
2023-11-13 22:32:20 +0530

    Made a ugly looking navbar

--- client/src/components/NavbarComponent.vue
+++ client/src/components/NavbarComponent.vue
@@ -1,6 +1,21 @@
 <template>
-  <nav>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
+  <nav
+    class="bg-slate-900 text-white p-3 mx-80 mt-4 rounded-xl shadow-lg shadow-slate-500/30 flex flex-row justify-center items-center"
+  >
+    <router-link
+      class="basis-1/4 font-sans text-lg cursor-pointer font-medium hover:font-bold mx-2"
+      to="/"
+      >recommender system</router-link
+    >
+    <div class="basis-1/2 flex flex-row">
+      <router-link to="/" class="mx-3 hover:underline">Discover Courses</router-link>
+      <router-link to="/" class="mx-3 hover:underline">My Courses</router-link>
+      <router-link to="/" class="mx-3 hover:underline">Recommendations</router-link>
+    </div>
+    <button
+      class="basis-1/6 bg-teal-800 p-2 rounded-lg shadow-md hover:shadow-lg hover:shadow-slate-500/30 hover:text-md"
+    >
+      Register
+    </button>
   </nav>
 </template>
--- client/src/pages/HomePage.vue
+++ client/src/pages/HomePage.vue
@@ -1,3 +1,5 @@
 <template>
-  <h1>This is the home page!</h1>
+  <div class="container mx-auto mt-4">
+    <h1 class="text-white">This is the home page!</h1>
+  </div>
 </template>
