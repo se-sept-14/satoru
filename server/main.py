253ff47829d06c5f902fb41fc43302fc83944ddb
kashifulhaque <haque.kashiful7@gmail.com>
2023-11-14 18:27:41 +0530

    Added CORS to FastAPI

--- server/main.py
+++ server/main.py
@@ -1,5 +1,6 @@
 from fastapi import FastAPI
 from contextlib import asynccontextmanager
+from fastapi.middleware.cors import CORSMiddleware
 
 from models.db import db_connection
 from apis.auth import auth_router
@@ -14,8 +15,19 @@ async def lifecycle(app: FastAPI):
   yield
   db_connection.close()
 
+# Allowed origins
+origins = ["*"]
+
 app = FastAPI(lifespan = lifecycle)
 app.include_router(auth_router, prefix = "/api/auth")
 app.include_router(profile_router, prefix = "/api/profile")
 app.include_router(course_router, prefix = "/api/course")
 app.include_router(review_router, prefix = "/api/review")
+
+app.add_middleware(
+  CORSMiddleware,
+  allow_origins = origins,
+  allow_credentials = True,
+  allow_methods = "*",
+  allow_headers = "*"
+)
