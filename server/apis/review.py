221809ec4679b88bcc66b4561d1a4d2a2f815a78
Tejas <gajra.tejas14@gmail.com>
2023-11-14 19:29:23 +0530

    flag a review

--- server/apis/review.py
+++ server/apis/review.py
@@ -20,3 +20,17 @@ async def create_review(review: ReviewsCreate, current_user: dict = Depends(deco
         return {"message": "Review created successfully", "review_id": review_instance.id}
     except Exception as e:
         raise HTTPException(status_code=500, detail=str(e))
+    
+@review_router.post("/flag/{review_id}")
+async def flag_review(review_id: int, current_user: dict = Depends(decode_token)):
+    try:
+        review = Reviews.get_by_id(review_id)
+        # Toggle the value of is_flagged
+        review.is_flagged = 1 if review.is_flagged == 0 else 0
+        review.save()
+        return {"message": "Review flag toggled successfully"}
+    except DoesNotExist:
+        raise HTTPException(status_code=404, detail="Review not found")
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=str(e))
+
