01c0224d351d3ee04968284f943d74db803ae0d0
Ubuntu <ubuntu@ip-172-26-1-5.ap-southeast-1.compute.internal>
2023-11-14 13:23:44 +0000

    updated reviews api

--- server/apis/review.py
+++ server/apis/review.py
@@ -20,3 +20,32 @@ async def create_review(review: ReviewsCreate, current_user: dict = Depends(deco
         return {"message": "Review created successfully", "review_id": review_instance.id}
     except Exception as e:
         raise HTTPException(status_code=500, detail=str(e))
+
+@router.get("/reviews")
+async def get_all_reviews():
+    try:
+        # Assuming Reviews is a SQLAlchemy model
+        reviews = Reviews.select().dicts()
+        return {"reviews": list(reviews)}
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=str(e))
+
+
+@router.delete("/delete/{review_id}")
+async def delete_review(review_id: int, current_user: dict = Depends(decode_token)):
+    try:
+        # Assuming Reviews is a SQLAlchemy model
+        review = Reviews.get(id=review_id)
+
+        # Check if the user trying to delete the review is the owner
+        if review.user != current_user["id"]:
+            raise HTTPException(status_code=403, detail="You don't have permission to delete this review")
+
+        # Delete the review
+        review.delete_instance()
+
+        return {"message": "Review deleted successfully"}
+    except Reviews.DoesNotExist:
+        raise HTTPException(status_code=404, detail="Review not found")
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=str(e))
