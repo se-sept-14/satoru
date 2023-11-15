bfa229a9e4b3536004dcb1d4f17feea99f61fdcd
kashifulhaque <haque.kashiful7@gmail.com>
2023-11-15 16:24:13 +0530

    Removed unused code

--- server/apis/admin.py
+++ server/apis/admin.py
@@ -33,23 +33,6 @@ async def get_all_students(current_user: dict = Depends(decode_token)):
     except Exception as e:
         raise HTTPException(status_code=500, detail=f"Something went wrong: {e}")
 
-
-# @admin_router.put("/alumni/{user_id}")
-# async def make_alumni(user_id: int, current_user: dict = Depends(decode_token)):
-#     is_admin = current_user["is_admin"]
-#     if not is_admin:
-#         raise HTTPException(status_code=403, detail="You are not an admin")
-#     try:
-#         student = Users.get_by_id(user_id)
-#     except Users.DoesNotExist:
-#         raise HTTPException(status_code=404, detail="User not found")
-#     if student.is_admin == 1:
-#         raise HTTPException(status_code=400, detail="Admin cannot be alumni")
-#     # print(student)
-#     student.is_alumni = 1 if student.is_alumni == 0 else 0
-#     student.save()
-#     return {"message": "Status of the student changed successfully"}
-
 @admin_router.get("/alumni/{user_id}")
 async def make_alumni(user_id: int, current_user: dict = Depends(decode_token)):
   is_admin = current_user["is_admin"]
