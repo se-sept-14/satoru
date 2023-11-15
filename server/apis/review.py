3664c14e031e924609e8046d44d38b941d02a519
kashifulhaque <haque.kashiful7@gmail.com>
2023-11-15 13:27:53 +0530

    Added tag review endpoint

--- server/apis/review.py
+++ server/apis/review.py
@@ -1,9 +1,8 @@
-from fastapi import APIRouter, Depends, HTTPException, Form
-from fastapi.security import OAuth2PasswordBearer
-from models.db import Users, Reviews
-from models.api import ReviewsCreate
-from utils.crypto import hash_password, decode_token
-from peewee import DoesNotExist
+from fastapi import APIRouter, Depends, HTTPException
+
+from utils.crypto import decode_token
+from models.api import ReviewsCreate, ReviewTagMapCreate
+from models.db import Reviews, Tags, ReviewTagMap, DoesNotExist
 
 review_router = APIRouter()
 
@@ -20,3 +19,31 @@ async def create_review(review: ReviewsCreate, current_user: dict = Depends(deco
         return {"message": "Review created successfully", "review_id": review_instance.id}
     except Exception as e:
         raise HTTPException(status_code=500, detail=str(e))
+
+@review_router.post("/tag")
+async def tag_review(review_tag: ReviewTagMapCreate, current_user: dict = Depends(decode_token)):
+  try:
+    is_admin = current_user["is_admin"]
+    if not is_admin:
+      return HTTPException(status_code = 403, detail = f"You are not an admin")
+    
+    review = Reviews.get(Reviews.id == review_tag.review_id)
+    tag = Tags.get(Tags.id == review_tag.tag_id)
+
+    if review and tag:
+      review_tag_map = ReviewTagMap.create(review = review, tag = tag)
+      review_tag_map.save()
+    else:
+      return HTTPException(status_code = 404, detail = f"Review ID {review_tag.review_id} or Tag ID {review_tag.tag_id} not found")
+    
+    return {
+      "message": "Review tagged successfully",
+      "data": {
+        "review_id": review_tag_map.review_id,
+        "tag_id": review_tag_map.tag_id
+      }
+    }
+  except DoesNotExist as dne:
+    raise HTTPException(status_code = 404, detail = f"No such review or tag")
+  except Exception as e:
+    raise HTTPException(status_code = 500, detail = f"{str(e)}")
