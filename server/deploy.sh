911f3759b6b205c21d1de54aa2ba050d2f1ab5e7
kashifulhaque <haque.kashiful7@gmail.com>
2023-11-17 10:58:30 +0530

    Update deploy scripts

--- server/deploy.sh
+++ server/deploy.sh
@@ -1,2 +1,9 @@
 #!/bin/bash
+
+# Stop running containers
+echo "Stopping running containers ..."
+docker compose down
+
+# Deploy the current version
+echo "Building and deploying containers ..."
 docker compose up -d
