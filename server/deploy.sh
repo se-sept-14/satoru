c9a0ce102351f8fd4cc049c2518c20c31ea89213
Kashif <haque.kashiful7@gmail.com>
2023-11-17 10:25:47 +0000

    Updated deploy script

--- server/deploy.sh
+++ server/deploy.sh
@@ -4,6 +4,18 @@
 echo "Stopping running containers ..."
 docker compose down
 
+# Delete old images
+echo "Delete old docker image ..."
+docker image rm server-api:latest
+
+# Build vite application
+echo "Building vite for prod ..."
+cd ../client
+npm install
+npm run build
+mv dist ../server
+cd ../server
+
 # Deploy the current version
 echo "Building and deploying containers ..."
 docker compose up -d
