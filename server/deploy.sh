f34c1f2be348c80e4634fc627f5508029449c57f
kashifulhaque <haque.kashiful7@gmail.com>
2023-11-25 23:19:26 +0530

    Added check for missing .env file

--- server/deploy.sh
+++ server/deploy.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
+# Check if .env file exists
+if [ ! -f .env ]; then
+  echo "Error: .env file not found. Please create the .env file before running this script."
+  exit 1
+fi
+
 # Stop running containers
 echo "Stopping running containers ..."
 docker compose down
