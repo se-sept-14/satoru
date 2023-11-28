44619ae77c1609681a72132ed3e56f8870f83ac5
Kashiful Haque <haque.kashiful7@gmail.com>
2023-11-28 14:19:34 +0530

    Update main.py

--- server/main.py
+++ server/main.py
@@ -25,8 +25,8 @@ async def lifecycle(app: FastAPI):
     db_connection.close()
 
 app = FastAPI(lifespan = lifecycle, redoc_url = None) # Disabled /redoc
-app.include_router(admin_router, prefix = "/api/admin")
 app.include_router(auth_router, prefix = "/api/auth")
+app.include_router(admin_router, prefix = "/api/admin")
 app.include_router(course_router, prefix = "/api/course")
 app.include_router(profile_router, prefix = "/api/profile")
 app.include_router(review_router, prefix = "/api/review")
