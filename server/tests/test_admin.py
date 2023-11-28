24dd84a28be1e84954bbe0d14cb6e657599a193d
kashifulhaque <haque.kashiful7@gmail.com>
2023-11-28 17:04:49 +0530

    Added a test for admin /all-students endpoint

---
+++ server/tests/test_admin.py
@@ -0,0 +1,35 @@
+from fastapi.testclient import TestClient
+
+from server.main import app
+from server.utils.crypto import create_access_token
+
+client = TestClient(app)
+
+
+# Test: Fetch all students (success)
+def test_get_all_students_success():
+  admin_user = { "id": 1, "is_admin": 1 }
+  token = create_access_token(admin_user) # Create a valid access token for the admin user
+  
+  # Make a request to endpoint with valid token
+  response = client.get("/api/admin/all-students", headers = { "Authorization": f"Bearer {token}" })
+
+  # Check that the response is 200 OK
+  assert response.status_code == 200
+
+  # Check that the response contains the "data" key
+  assert "data" in response.json()
+
+# Test: Fetch all students (failure)
+def test_get_all_students_unauthorized():
+  regular_user = {"id": 2, "is_admin": 0 }
+  token = create_access_token(regular_user) # Create a valid access token for the non-admin user
+
+  # Make a request to endpoint with non-admin user's token
+  response = client.get("api/admin/all-students", headers = { "Authorization": f"Bearer {token}" })
+
+  # Check that the response code is 403 (Forbidden)
+  assert response.status_code == 403
+
+  # Check that the response contains the expected error message
+  assert "You are not an admin" in response.text
