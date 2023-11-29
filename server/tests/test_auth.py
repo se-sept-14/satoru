b87b0bd385daccbc6f144f92cf2e759f348156c1
kashifulhaque <haque.kashiful7@gmail.com>
2023-11-29 11:58:37 +0530

    Added test for existing user registration

--- server/tests/test_auth.py
+++ server/tests/test_auth.py
@@ -1,6 +1,7 @@
 from fastapi.testclient import TestClient
 
 from server.main import app
+from server.utils.random import random_string
 
 client = TestClient(app)
 
@@ -8,9 +9,9 @@ client = TestClient(app)
 # Test: New user registration (success)
 def test_register_user_success():
   user_data = {
-    "email": "test@pickmycourse.online",
-    "username": "testuser",
-    "password": "test-password"
+      "email": random_string() + "@pickmycourse.online",
+      "username": random_string(),
+      "password": random_string(),
   }
 
   response = client.post("api/auth/register", json = user_data)
@@ -18,3 +19,15 @@ def test_register_user_success():
   assert response.status_code == 200
   assert response.json()["email"] == user_data["email"]
   assert response.json()["username"] == user_data["username"]
+
+# Test: New user registration [existing email or username] (failure)
+def test_register_user_existing_username_email_failure():
+  user_data = {
+    "email": "test@pickmycourse.online",
+    "username": "testuser",
+    "password": "test-password"
+  }
+  response = client.post("api/auth/register", json = user_data)
+
+  assert response.status_code == 400
+  assert "Email or Username already in use" in response.text
