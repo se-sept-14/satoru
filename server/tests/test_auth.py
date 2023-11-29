be6f3ca26b1c59463350258e610f862618350726
kashifulhaque <haque.kashiful7@gmail.com>
2023-11-29 12:09:02 +0530

    Added tests for logging in a user

--- server/tests/test_auth.py
+++ server/tests/test_auth.py
@@ -1,6 +1,7 @@
 from fastapi.testclient import TestClient
 
 from server.main import app
+from server.utils.crypto import decode_token
 from server.utils.random import random_string
 
 client = TestClient(app)
@@ -31,3 +32,15 @@ def test_register_user_existing_username_email_failure():
 
   assert response.status_code == 400
   assert "Email or Username already in use" in response.text
+
+
+# Test: Logging in a user (success)
+def test_login_user_success():
+  user_data = {
+    "username": "testuser",
+    "password": "test-password"
+  }
+  response = client.post("api/auth/login", data = user_data)
+
+  assert response.status_code == 200
+  assert "access_token" in response.json() and "token_type" in response.json()
