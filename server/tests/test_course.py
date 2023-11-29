42c49da733fdaa538b72d62b53f576739b14b14d
kashifulhaque <haque.kashiful7@gmail.com>
2023-11-29 12:32:07 +0530

    Added tests for fetching a nonexistent course by ID

--- server/tests/test_course.py
+++ server/tests/test_course.py
@@ -7,11 +7,21 @@ client = TestClient(app)
 
 
 # Test: Fetch a course by ID (success)
-def test_fetch_course_by_id():
+def test_fetch_course_by_id_success():
   user = { "id": 2, "is_admin": 0 }
   token = create_access_token(user)
   course_id = 1
   response = client.get(f"api/course/id/{course_id}", headers = { "Authorization": f"Bearer {token}" })
-  
+
   assert response.status_code == 200
   assert "data" in response.json()
+
+# Test: Fetch a course by ID (failure)
+def test_fetch_course_by_id_failure():
+  user = { "id": 2, "is_admin": 0 }
+  token = create_access_token(user)
+  course_id = 1000000 # Course ID one million
+  response = client.get(f"api/course/id/{course_id}", headers = { "Authorization": f"Bearer {token}" })
+
+  assert response.status_code == 404
+  assert f"No course with id {course_id}" in response.text
