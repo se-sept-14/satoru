546b3d4d3bdeea694d24019b18ba76442d6a252e
kashifulhaque <haque.kashiful7@gmail.com>
2023-11-30 00:30:17 +0530

    Commented admin.py code 📄

--- server/apis/admin.py
+++ server/apis/admin.py
@@ -9,7 +9,8 @@ admin_router = APIRouter(tags = ["Admin 🤵"])
 
 @admin_router.get("/all-students", summary = "Fetch a list of all students 🧑‍🤝‍🧑")
 async def get_all_students(current_user: dict = Depends(decode_token)):
-  is_admin = current_user["is_admin"] # Check if the current user is an admin
+  # Check if the current user is an admin
+  is_admin = current_user["is_admin"]
   if not is_admin:
     raise HTTPException(status_code = 403, detail = "You are not an admin ⛔")
 
@@ -17,9 +18,9 @@ async def get_all_students(current_user: dict = Depends(decode_token)):
     # Fetch all users who are not admin
     users = Users.select().where(Users.is_admin == 0)
 
+    # Get the profile of all students and append them to a list
     students = []
     for user in users:
-      user_dict = model_to_dict(user)
       student = Students.get_or_none(Students.user_id == user.id)
       student_dict = model_to_dict(student, exclude = [Users.password]) if student is not None else {}
       students.append({ **student_dict })
@@ -31,19 +32,23 @@ async def get_all_students(current_user: dict = Depends(decode_token)):
 
 @admin_router.get("/alumni/{user_id}", summary = "Make a student alumni 🎓")
 async def make_alumni(user_id: int, current_user: dict = Depends(decode_token)):
+  # Check if the current user is an admin
   is_admin = current_user["is_admin"]
   if not is_admin:
     raise HTTPException(status_code = 403, detail = "You are not an admin ⛔")
   
   try:
+    # Check if the given user ID exist in the DB or not
     user = Users.get_or_none(Users.id == user_id)
     if not user:
       raise HTTPException(status_code = 404, detail = f"User with ID {user_id} does not exist ❌")
     
+    # If that user exist, check if they have a complete profile or not
     student = StudentAboutMe.get_or_none(StudentAboutMe.user_id == user.id)
     if student is None:
       raise HTTPException(status_code = 404, detail = "Student has not completed the profile 🧑‍🎓")
     
+    # If both of the previous conditions are true, update the `is_alumni` status of that student to `True``
     query = StudentAboutMe.update(is_alumni = True).where(StudentAboutMe.user_id == user.id)
     query.execute()
   except HTTPException as e:
