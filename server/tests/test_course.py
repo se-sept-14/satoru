0cb40fd5275fc6f803412409e796f3b793b30ddd
kashifulhaque <haque.kashiful7@gmail.com>
2023-11-29 14:50:41 +0530

    Added test for delete course non-admin user

--- server/tests/test_course.py
+++ server/tests/test_course.py
@@ -84,3 +84,50 @@ def test_create_course_nonadmin_user_failure():
 
   assert response.status_code == 403
   assert "You are not an admin" in response.text
+
+# # Test: Delete a course (success)
+# def test_delete_course_success():
+#   admin_user = { "id": 1, "is_admin": 1 }
+#   token = create_access_token(admin_user)
+#   course_to_delete = 6
+#
+#   response = client.delete(f"api/course/{course_to_delete}", headers = { "Authorization": f"Bearer {token}" })
+#
+#   assert response.status_code == 200
+#   assert "message" in response.json() and "data" in response.json() and f"Course with ID {course_to_delete} deleted successfully" in response.json()["message"]
+
+# Test: Delete a course (failure)
+def test_delete_course_nonadmin_user_failure():
+  nonadmin_user = { "id": 2, "is_admin": 0 }
+  token = create_access_token(nonadmin_user)
+  course_to_delete = 6
+
+  response = client.delete(f"api/course/{course_to_delete}", headers = { "Authorization": f"Bearer {token}" })
+
+  assert response.status_code == 403
+  assert "You are not an admin" in response.text
+
+# Test: Add a new course [non-admin user] (failure)
+# def test_create_course_nonadmin_user_failure():
+#   nonadmin_user = { "id": 2, "is_admin": 0 }
+#   token = create_access_token(nonadmin_user)
+#
+#   # Test valid data
+#   valid_data = {
+#     "name": "Test Course",
+#     "code": "TC101",
+#     "price": 4000,
+#     "credits": 3,
+#     "description": "Test course description",
+#     "corequisite": "None",
+#     "prerequisites": "None",
+#     "hours_per_week": "15",
+#     "instructor_name": "John Doe",
+#     "instructor_picture": "http://example.com/johndoe.jpg",
+#     "tags": ["tag1", "tag2"]
+#   }
+#
+#   response = client.post("api/course/", json = valid_data, headers = { "Authorization": f"Bearer {token}" })
+#
+#   assert response.status_code == 403
+#   assert "You are not an admin" in response.text
