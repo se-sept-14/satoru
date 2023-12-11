3c21cb1b0b37990f5fb28a9cb99564b5b1ed563f
kashifulhaque <haque.kashiful7@gmail.com>
2023-12-11 10:11:34 +0530

    Changed the API base url 🍃

--- client/src/stores/AuthStore.js
+++ client/src/stores/AuthStore.js
@@ -4,7 +4,7 @@ import { defineStore } from "pinia";
 export const useAuthStore = defineStore("authStore", {
   state: () => ({
     api: {
-      server: window.location.hostname == 'localhost' || window.location.hostname == '127.0.0.1' ? 'http://pickmycourse.online' : '',
+      server: 'https://api.pickmycourse.online',
       endpoints: {
         auth: {
           login: "/api/auth/login",
