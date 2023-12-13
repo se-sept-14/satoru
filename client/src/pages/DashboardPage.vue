892b46c53a0eb30d444e8a64f03e5849006be2a2
DotSlash-A <mirana.2358@gmail.com>
2023-12-13 08:06:27 +0000

    searchbar

--- client/src/pages/DashboardPage.vue
+++ client/src/pages/DashboardPage.vue
@@ -1,13 +1,38 @@
 <template>
   <div>
-    <h1 class="m-8 text-4xl text-white">You are now logged in</h1>
+    <div class="flex items-center justify-center my-8">
+      <div class="relative">
+        <input
+          type="text"
+          class="bg-black text-white border border-gray-700 rounded-full py-3 px-6 pl-14 placeholder-#000101 focus:outline-none focus:shadow-outline w-96"
+          placeholder="Search..."
+        />
+        <div class="absolute inset-y-0 left-0 flex items-center pl-5">
+          <svg
+            class="w-3/4 h-4 text-gray-500 dark:text-gray-400"
+            aria-hidden="true"
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 20 20"
+          >
+            <path
+              stroke="currentColor"
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              stroke-width="2"
+              d="m19 19-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z"
+            />
+          </svg>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
 export default {
-  name: "DashboardPage"
-}
+  name: "DashboardPage",
+};
 </script>
 
 <style scoped></style>
