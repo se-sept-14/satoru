9a8a4ef7070469cd2978e4f91dc7d17b478e2944
kashifulhaque <haque.kashiful7@gmail.com>
2023-12-13 23:35:57 +0530

    Added docker compose service for vite client

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,15 @@
 #!/bin/bash
 
+# Stop existing (and running continers)
+echo "Stopping existing containers ..."
+docker compose down
+
 # Check if .env file exists
 if [ ! -f ./server/.env ]; then
   echo "Error: .env file not found. Please create the .env file inside `server` directory before running this script."
   exit 1
 fi
+
+# Run the docker compose service
+echo "Deploying ..."
+docker compose up --build -d
