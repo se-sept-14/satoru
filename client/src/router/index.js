2d7aa89df12e35cdedd7fd8673a017784a1a6cae
DotSlash-A <mirana.2358@gmail.com>
2023-12-13 09:17:44 +0000

    fixed dashboard

--- client/src/router/index.js
+++ client/src/router/index.js
@@ -10,26 +10,26 @@ export default createRouter({
       name: "HomePage",
       component: HomePage,
     },
-    // {
-    //   path: "/login",
-    //   name: "LoginPage",
-    //   component: () => import("@/pages/LoginPage.vue"),
-    // },
     {
       path: "/login",
       name: "LoginPage",
       component: () => import("@/pages/LoginPage.vue"),
-      beforeEnter: (to, from, next) => {
-        // Check if the user is already logged in
-        if (store.getters["auth/isLoggedIn"]) {
-          // If they are, redirect to the dashboard
-          next("/dashboard");
-        } else {
-          // If not, continue to the login page
-          next();
-        }
-      },
     },
+    // {
+    //   path: "/login",
+    //   name: "LoginPage",
+    //   component: () => import("@/pages/LoginPage.vue"),
+    //   beforeEnter: (to, from, next) => {
+    //     // Check if the user is already logged in
+    //     if (store.getters["auth/isLoggedIn"]) {
+    //       // If they are, redirect to the dashboard
+    //       next("/dashboard");
+    //     } else {
+    //       // If not, continue to the login page
+    //       next();
+    //     }
+    //   },
+    // },
     {
       path: "/register",
       name: "RegisterPage",
