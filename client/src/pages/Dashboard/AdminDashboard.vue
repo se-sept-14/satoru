89d493897994147b8fc61271630e5920f0eaed1e
kashifulhaque <haque.kashiful7@gmail.com>
2023-12-16 16:40:46 +0530

    Made a barebones admin dashboard

--- client/src/pages/Dashboard/AdminDashboard.vue
+++ client/src/pages/Dashboard/AdminDashboard.vue
@@ -1,6 +1,36 @@
 <template>
   <div>
     <navbar />
+
+    <div class="flex flex-row text-white w-[640px] mx-auto justify-center">
+      <!-- Left column -->
+      <div class="flex flex-col">
+        <div class="border-2 border-teal-500 p-12 rounded-md">
+          <p class="font-light text-xl">Students</p>
+          <p class="font-extralight text-sm italic mt-2">
+            List of all enrolled students
+          </p>
+
+          <button class="bg-emerald-800 px-4 py-2 rounded-full mt-4">
+            View students
+          </button>
+        </div>
+      </div>
+
+      <!-- Right column -->
+      <div class="flex flex-col ml-4">
+        <div class="border-2 border-teal-500 p-12 rounded-md">
+          <p class="font-light text-xl">Courses</p>
+          <p class="font-extralight text-sm italic mt-2">
+            List of all available courses
+          </p>
+
+          <button class="bg-cyan-400 text-black px-4 py-2 rounded-full mt-4">
+            View courses
+          </button>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
