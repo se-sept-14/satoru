258e16bc8b0cbe57108e557a8f2058d17f1ec1df
kashifulhaque <haque.kashiful7@gmail.com>
2023-12-16 21:16:40 +0530

    Update .env.example

--- deploy.sh
+++ deploy.sh
@@ -6,7 +6,7 @@ docker compose down
 
 # Check if .env file exists
 if [ ! -f ./server/.env ]; then
-  echo "Error: .env file not found. Please create the .env file inside `server` directory before running this script."
+  echo "Error: .env file not found. Please create the .env file inside \`server\` directory before running this script. Check the README"
   exit 1
 fi
 
