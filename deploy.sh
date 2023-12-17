c2fe4428970c4e428a278ed0a2b59acdd290a6bc
kashifulhaque <haque.kashiful7@gmail.com>
2023-12-17 12:22:42 +0530

    Streamlined deploymeny

--- deploy.sh
+++ deploy.sh
@@ -4,9 +4,15 @@
 echo "Stopping existing containers ..."
 docker compose down
 
-# Check if .env file exists
+# Check if .env file exists in 'server' directory
 if [ ! -f ./server/.env ]; then
   echo "Error: .env file not found. Please create the .env file inside \`server\` directory before running this script. Check the README"
+  exit 1
+fi
+
+# Check if .env file exists in 'client' directory
+if [ ! -f ./client/.env ]; then
+  echo "Error: .env file not found. Please create the .env file inside \`client\` directory before running this script. Check the README"
   exit 1
 fi
 
