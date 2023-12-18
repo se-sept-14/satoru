5ad20ebbda270af1cd7ade9618b42ea5ca01cba2
DotSlash-A <mirana.2358@gmail.com>
2023-12-18 12:53:10 +0000

    snow, a lot of it.

--- client/src/pages/Auth/LoginPage.vue
+++ client/src/pages/Auth/LoginPage.vue
@@ -116,9 +116,11 @@ export default {
   methods: {
     async login(e) {
       e.preventDefault(); // Prevents the default behaviour of submit button (to refresh the page)
-      
-      if (this.userData.username.length == 0) this.userDataError.username = true;
-      if (this.userData.password.length == 0) this.userDataError.password = true;
+
+      if (this.userData.username.length == 0)
+        this.userDataError.username = true;
+      if (this.userData.password.length == 0)
+        this.userDataError.password = true;
 
       if (!this.userDataError.username && !this.userDataError.password) {
         const user = await this.authStore.login(this.userData);
@@ -137,7 +139,7 @@ export default {
             localStorage.setItem("token_type", user.token_type);
             localStorage.setItem("access_token", user.access_token);
 
-            if(await this.authStore.isAdmin()) {
+            if (await this.authStore.isAdmin()) {
               this.$router.push("/admin-dashboard");
             } else {
               this.$router.push("/dashboard");
@@ -157,10 +159,69 @@ export default {
     },
   },
   mounted() {
-    if(this.authStore.isLoggedIn()) {
+    if (this.authStore.isLoggedIn()) {
       this.$router.push("/dashboard");
       return;
     }
+
+    const NUMBER_OF_SNOWFLAKES = 300;
+    const MAX_SNOWFLAKE_SIZE = 5;
+    const MAX_SNOWFLAKE_SPEED = 2;
+    const SNOWFLAKE_COLOUR = "#ddd";
+    const snowflakes = [];
+
+    const canvas = document.createElement("canvas");
+    canvas.style.position = "absolute";
+    canvas.style.pointerEvents = "none";
+    canvas.style.top = "0px";
+    canvas.width = window.innerWidth;
+    canvas.height = window.innerHeight;
+    document.body.appendChild(canvas);
+
+    const ctx = canvas.getContext("2d");
+
+    const createSnowflake = () => ({
+      x: Math.random() * canvas.width,
+      y: Math.random() * canvas.height,
+      radius: Math.floor(Math.random() * MAX_SNOWFLAKE_SIZE) + 1,
+      color: SNOWFLAKE_COLOUR,
+      speed: Math.random() * MAX_SNOWFLAKE_SPEED + 1,
+      sway: Math.random() - 0.5, // next
+    });
+
+    const drawSnowflake = (snowflake) => {
+      ctx.beginPath();
+      ctx.arc(snowflake.x, snowflake.y, snowflake.radius, 0, Math.PI * 2);
+      ctx.fillStyle = snowflake.color;
+      ctx.fill();
+      ctx.closePath();
+    };
+
+    const updateSnowflake = (snowflake) => {
+      snowflake.y += snowflake.speed;
+      snowflake.x += snowflake.sway; // next
+      if (snowflake.y > canvas.height) {
+        Object.assign(snowflake, createSnowflake());
+      }
+    };
+
+    const animate = () => {
+      ctx.clearRect(0, 0, canvas.width, canvas.height);
+
+      snowflakes.forEach((snowflake) => {
+        updateSnowflake(snowflake);
+        drawSnowflake(snowflake);
+      });
+
+      requestAnimationFrame(animate);
+    };
+
+    for (let i = 0; i < NUMBER_OF_SNOWFLAKES; i++) {
+      snowflakes.push(createSnowflake());
+    }
+
+    // setInterval(animate, 15);
+    animate();
   },
 };
 </script>
